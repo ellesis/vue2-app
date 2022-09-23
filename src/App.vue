31e18dcd6e247c04381b8c217d01633389a8dde7
Lisa Han <ellesis@gmail.com>
2022-09-23 05:12:45 -0800

    Add highchart in locally

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <div>
+      <h1>vue2-app</h1>
+      <img
+        alt="Vue logo"
+        src="./assets/logo.png"
+      />
+    </div>
+    <hr />
+
+    <!-- highchart -->
+    <div>
+      <h1>Highchart</h1>
+      <RiskEventChart />
+    </div>
+    <hr />
+
+    <!-- Font awesome -->
+    <div>
+      <h1>Font awesome - npm i fontawesome</h1>
+      <h2>
+        <font-awesome-icon icon="fa-solid fa-user-secret" />
+        <font-awesome-icon icon="fa-solid fa-circle-question" />
+        <font-awesome-icon icon="fa-solid fa-rotate" />
+        <font-awesome-icon icon="fa-solid fa-exclamation-triangle" />
+
+        <font-awesome-icon icon="fa-solid fa-video" />
+        <font-awesome-icon icon="fa-solid fa-camera" />
+
+        <font-awesome-icon icon="fa-solid fa-battery-three-quarters" />
+      </h2>
+    </div>
+    <hr />
+
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <div>Font awesome</div>
-    <h2>
-      <font-awesome-icon icon="fa-solid fa-user-secret" />
-      <font-awesome-icon icon="fa-solid fa-circle-question" />
-      <font-awesome-icon icon="fa-solid fa-rotate" />
-      <font-awesome-icon icon="fa-solid fa-exclamation-triangle" />
-
-      <font-awesome-icon icon="fa-solid fa-video" />
-      <font-awesome-icon icon="fa-solid fa-camera" />
-
-      <font-awesome-icon icon="fa-solid fa-battery-three-quarters" />
-    </h2>
-
-    <i class="fa-solid fa-circle-question"></i>
   </div>
 </template>
 
 <script>
   import HelloWorld from './components/HelloWorld.vue'
+  import RiskEventChart from './components/charts/RiskEventChart.vue'
 
   export default {
     name: 'App',
     components: {
-      HelloWorld
+      HelloWorld,
+      RiskEventChart
     }
   }
 </script>
---
+++ src/components/charts/RiskEventChart.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <Chart :options="eventRiskChartOptions"></Chart>
+  </div>
+</template>
+
+<script>
+  import { Chart } from 'highcharts-vue'
+
+  export default {
+    components: {
+      Chart
+    },
+    data() {
+      return {
+        eventRiskChartOptions: {
+          chart: {
+            type: 'line',
+            borderColor: '#ddd',
+            borderWidth: 1
+          },
+          title: {
+            // text: $.i18n.prop('MESS_UnsafeDrivingEventsPerDay')
+            text: 'UnsafeDrivingEventsPerDay'
+          },
+          colors: ['red'],
+          legend: {
+            enabled: false
+          },
+          xAxis: {
+            categories: ['1', '2'], //xAxisList, //date
+            title: {
+              // text: $.i18n.prop('MESS_Days')
+              test: 'Days'
+            }
+          },
+          yAxis: {
+            min: 0,
+            allowDecimals: false,
+            title: {
+              // text: $.i18n.prop('MESS_Events')
+              text: 'Events'
+            }
+          },
+          plotOptions: {
+            line: {
+              dataLabels: {
+                enabled: true
+              }
+            },
+            series: {
+              states: {
+                hover: {
+                  enabled: false
+                }
+              }
+            }
+          },
+          series: [
+            {
+              // name: riskLevelText,
+              // data: graphData
+              data: [1, 2, 3, 4]
+            }
+          ]
+        }
+      }
+    }
+  }
+</script>
